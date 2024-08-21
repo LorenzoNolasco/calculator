ebe698932f46524122f7bafcb44f678112ffc418
Lorenzo Nolasco <Lorenzonolasco23@gmail.com>
2024-08-21 10:51:59 -0600

    made an index.html, style.css, and a brain.js

---
+++ assets/js/brain.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("userInput");
+
+const calculate = document
+  .querySelectorAll(".operations")
+  .forEach(function (item) {
+    console.log(item);
+    item.addEventListener("click", function (e) {
+    });
+  });
+
+const number = document.querySelectorAll(".numbers").forEach(function(item) {
+    item.addEventListener("click", function(e) {
+        
+    })
+})
